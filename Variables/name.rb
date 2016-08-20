265fd72e1bcb09b8851c50d13c3341bfd3e16822
TzuYuan <tzuyuanliu0830@gmail.com>
2016-08-20 18:33:07 +0800

    add full name to name.rb

--- Variables/name.rb
+++ Variables/name.rb
@@ -1,7 +1,10 @@
-puts "Hi, What is your name?"
-name = gets.chomp
-puts "#{name}! What a nice name!"
+puts "Hi, What is your first name?"
+first_name = gets.chomp
+puts "And what is your last name?"
+last_name = gets.chomp
+puts "#{first_name} #{last_name}! What a nice name!"
+fullname = first_name + " "+ last_name
 
 10.times do |n|
-	puts name
+	puts fullname
 end
