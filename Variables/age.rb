0524abaef0c299c5e54e7f9a37099f2f2e08ed9f
TzuYuan <tzuyuanliu0830@gmail.com>
2016-08-20 18:25:15 +0800

    take user's age and print out their age after 10, 20, 30, and 40 years

---
+++ Variables/age.rb
@@ -0,0 +1,13 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "In #{age} years you will be:"
+puts age + 10
+puts "In #{age} years you will be:"
+puts age + 20
+puts "In #{age} years you will be:"
+puts age + 30
+puts "In #{age} years you will be:"
+puts age + 40
+
+
