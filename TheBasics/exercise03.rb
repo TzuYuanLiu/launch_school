d8231ed0ea797e16b93719b335af08743ed0610f
TzuYuan <tzuyuanliu0830@gmail.com>
2016-08-20 17:18:45 +0800

    puts year of each movie

---
+++ TheBasics/exercise03.rb
@@ -0,0 +1,14 @@
+movies = { :JAWS => 1975, 
+					 :Eternal_Sunshine_of_the_Spotless_Mind => 2004, 
+					 :'The_Hobbit:_The_Desolation_of_Smaug' => 2013, 
+					 :'A.I._Artificial_Intelligence' => 2001, 
+					 :Raiders_of_the_Lost_Ark => 1981 }
+
+puts movies[:JAWS]
+puts movies[:Eternal_Sunshine_of_the_Spotless_Mind]
+puts movies[:'The_Hobbit:_The_Desolation_of_Smaug']
+puts movies[:'A.I._Artificial_Intelligence']
+puts movies[:Raiders_of_the_Lost_Ark]
+
+
+
